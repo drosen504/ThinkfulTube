a98b84858514ff88424b438dac67233b9bc5ff21
Dan Rosen <drosen84@gmail.com>
2018-01-09 13:49:25 -0600

    thumbs clickable but isnt picking up videoID

--- app.js
+++ app.js
@@ -2,30 +2,33 @@
 /* global $ */
 
 const YT_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
+const API_KEY = 'AIzaSyA13I4b3i6USOCq0Z5yPX1VsCYeE0dzE-E'
 
 function getDataFromApi(searchTerm, callback) {
   const query = {
     part: 'snippet',
     q: searchTerm,
-    key: 'AIzaSyA13I4b3i6USOCq0Z5yPX1VsCYeE0dzE-E'
+    key: API_KEY
     
   };
   $.getJSON(YT_SEARCH_URL, query, callback);
-  console.log($.getJSON(YT_SEARCH_URL, query));
 }
 
 function renderResult(result) {
   console.log(result);
+  //   console.log(result.id.videoID);
   return `
     <div>
-      <a href= 'https://www.youtube.com/watch?v=${result.id.videoID}' target="_blank">${result.snippet.title}</a>
+      <a href= 'https://www.youtube.com/watch?v=${result.id.videoID}' target= '_blank'> ${result.snippet.title}
+      <img class='js-thumbnail' src='${result.snippet.thumbnails.medium.url}'</a>
 
     </div>
   `;
 }
 
 function displayYTSearchData(data) {
-  const results = data.items.map((item, index) => renderResult(item));
+  const results = data.items.map((item) => renderResult(item));
+  console.log(results);
   $('.js-search-results').html(results);
 }
 
