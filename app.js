6a4251ee927b0b56fa8c5187b3c66ae766794fcc
Dan Rosen <drosen84@gmail.com>
2018-01-12 11:12:40 -0600

    reverted to earlier version of code without generator

--- app.js
+++ app.js
@@ -18,6 +18,44 @@ function getDataFromApi(searchTerm, callback, pageToken) {
   lastSearch = query.q;
 }
 
+function renderResult(result) {
+  console.log(result);
+  return `
+  </div><br>   
+    <div>
+    <a href = 'https://www.youtube.com/watch?v=${result.id.videoId}' target= '_blank'>${result.snippet.title}<br>
+      <img class='js-thumbnail' src='${result.snippet.thumbnails.medium.url}'
+    </a><br>
+    <a href = 'https://www.youtube.com/channel/${result.snippet.channelId}/videos' target= '_blank'>More videos from ${result.snippet.channelTitle}.</a> 
+    </div>
+    <br>
+  `;
+}
+
+function renderMoreButton(token) {
+  $('#more-results').html(`
+    <button id='more-vids' type='button' data-token='${token}'>More results</button>
+    `);
+}
+
+function renderPrevButton(token) {
+  $('#prev-results').html(`
+  <button id='prev-vids' type='button' data-token='${token}'>Previous results</button>
+  `);
+}
+
+function displayYTSearchData(data) {
+  const results = data.items.map((item) => renderResult(item));
+  let nextPageToken = data.nextPageToken;
+  $('.js-search-results').html(results);
+  console.log($('#prev-vids').data('token'));
+  if ($('#prev-vids').data('token') === undefined) {
+    $('#prev-vids').data('#prev-vides', 'token', data.prevPageToken);
+  }
+  renderMoreButton(nextPageToken);
+  
+} 
+
 function watchSubmit() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
@@ -26,24 +64,20 @@ function watchSubmit() {
     console.log(query);
     // clear out the input
     queryTarget.val('');
-    getDataFromApi(query, generator.displayYTSearchData);
+    getDataFromApi(query, displayYTSearchData);
   });
 }
 
 //waits for more results button to be clicked and triggers new API call be made.
 $('#more-results').click(event => {
-  getDataFromApi(lastSearch, generator.displayYTSearchData, $('#more-vids').data('token'));
+  getDataFromApi(lastSearch, displayYTSearchData, $('#more-vids').data('token'));
 });
 
-
-
+$('#prev-results').click(event => {
+  getDataFromApi(lastSearch, displayYTSearchData, $('#prev-vids').data('token'));
+});
 
 $(watchSubmit);
-
-
-// $('#prev-results').click(event => {
-//   getDataFromApi(lastSearch, displayYTSearchData, $('#prev-vids').data('token'));
-// });
 
 // function renderPrevButton(token) {
 //   $('#prev-results').html(`
